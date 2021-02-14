23ec67f198cd2512bcd07e62c311555e0c59b7b6
portfolio-y0711 <portfolio-y0711@gmail.com>
2021-02-15 00:24:58 +0900

    add controller unit test with mock

--- src/test/kotlin/com/webapp/tdd/OutsideControllerUnitTest.kt
+++ src/test/kotlin/com/webapp/tdd/OutsideControllerUnitTest.kt
@@ -4,8 +4,6 @@ import com.ninjasquad.springmockk.MockkBean
 import com.webapp.tdd.entity.Outside
 import com.webapp.tdd.service.OutsideService
 import io.mockk.every
-import io.mockk.junit5.MockKExtension
-import io.mockk.mockk
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
@@ -13,23 +11,22 @@ import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
-import org.junit.jupiter.api.extension.ExtendWith
-import org.mockito.junit.jupiter.MockitoExtension
-import org.springframework.boot.test.context.TestConfiguration
-import org.springframework.context.annotation.Bean
 
 @WebMvcTest(OutsideController::class)
-@ExtendWith(MockKExtension::class)
-internal class OutsideControllerTest(@Autowired val mockMvc: MockMvc) {
+internal class OutsideControllerUnitTest(@Autowired val mockMvc: MockMvc) {
 
-    @Autowired
+//    @Autowired
+//    private lateinit var service: OutsideService
+//
+//    @TestConfiguration
+//    class ControllerTestConfig {
+//        @Bean
+//        fun service() = mockk<OutsideService>()
+//    }
+
+    @MockkBean
     private lateinit var service: OutsideService
 
-    @TestConfiguration
-    class ControllerTestConfig {
-        @Bean
-        fun service() = mockk<OutsideService>()
-    }
 
     @Test
     fun `GET_api_outside should return`() {
